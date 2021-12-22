0b11cad3d5c209fd8a37915b3623fed8abbeccba
Kartik Vedi <44085167+kartikvedi@users.noreply.github.com>
2021-12-22 16:51:38 +0530

    Add files via upload

---
+++ gif.py
@@ -0,0 +1,7 @@
+# pip install moviepy
+from moviepy.editor import VideoFileClip
+from tkinter.filedialog import *
+
+video=askopenfilename()
+clip=VideoFileClip(video)
+clip.write_gif("mygif.gif",fps=10)
